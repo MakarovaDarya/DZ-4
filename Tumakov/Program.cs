65cc1c67a226072282997ea83faf297bf6a726f9
Makarova Darya <makarovadarya2704@gmail.com>
2022-10-09 17:29:37 +0300

    дз-4

--- Tumakov/Program.cs
+++ Tumakov/Program.cs
@@ -10,24 +10,7 @@ namespace Tumakov
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите размерность массива?");
-            int razmer = Convert.ToInt16(Console.ReadLine());
-
-            double[] massive = new double[razmer];
-            double summa;
-            summa = 0;
-            var digitsRow = Console.ReadLine();
-            var digits = digitsRow.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            for (int i = 0; i < razmer; i++)
-            {
-
-                foreach (var digit in digits)
-                {
-                    massive[i] = Convert.ToDouble(digit);
-
-                }
-            }
-            Console.WriteLine(String.Join(",",massive));
+            
         }
     }
 }
